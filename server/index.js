9c94a2a0ca72367702c36dc230d5bacb6e00315f
Ankur Akash <07ankur007@gmail.com>
2024-08-22 14:42:30 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
 	cors({
-		origin:"http://localhost:3000",
+		origin:"https://learnsphere-5xsgn8ii0-ankur-akashs-projects.vercel.app",
 		credentials:true,
 	})
 )
